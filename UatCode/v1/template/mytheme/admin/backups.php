acb423384d3ba52f70605ee59c7381f225e01d1f
allen <980227856@qq.com>
2022-02-25 16:40:19 +0800

    commit

---
+++ UatCode/v1/template/mytheme/admin/backups.php
@@ -0,0 +1,44 @@
+<?php
+
+require "config.php";
+require "chk.php";
+if (power("alevel") != 3) {
+	exit("error");
+}
+$settingfile = 0;
+if (isset($_GET["cid"])) {
+	$cid = intval($_GET["cid"]);
+	$fidchannel = getchannelcache($cid);
+	if ($fidchannel["ckind"] == 5) {
+		$settingfile = 1;
+	} elseif ($fidchannel["ckind"] == 1) {
+		$settingfile = 3;
+	} elseif ($fidchannel["ckind"] == 2) {
+		$settingfile = 3;
+	} else {
+		$settingfile = 1;
+	}
+} else {
+	$cid = 0;
+	$settingfile = 1;
+}
+$sitetime = date("YmdHis");
+$filename = $sitetime . ".json";
+$encoded_filename = urlencode($filename);
+$encoded_filename = str_replace("+", "%20", $encoded_filename);
+header("Content-Type: application/octet-stream");
+if (preg_match("/MSIE/", $_SERVER["HTTP_USER_AGENT"])) {
+	header("Content-Disposition:  attachment; filename=\"" . $encoded_filename . "\"");
+} elseif (preg_match("/Firefox/", $_SERVER["HTTP_USER_AGENT"])) {
+	header("Content-Disposition: attachment; filename*=\"utf8" . $filename . "\"");
+} else {
+	header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
+}
+$result = array();
+$result["str"] = $GLOBALS["db"]->getAllRes("SELECT id,strvalue FROM `sm_str`");
+$result["channel"] = $GLOBALS["db"]->getAllRes("SELECT cid,cname FROM `sm_channel`");
+print_r(json_encode($result));
+// if (!isset($check_host) || !isset($check_info)) {
+// 	echo "您的域名未授权并被记录，请不要修改核心文件名称或尝试破解！！！";
+// 	exit;
+// }
---
+++ UatCode/v1/template/mytheme/admin/str/editpost.php
@@ -0,0 +1,88 @@
+<?php
+
+if (!defined("admin")) {
+	exit;
+}
+checktoken();
+$msg = "修改成功";
+$GLOBALS["db"]->begin();
+foreach ($_POST as $key => $value) {
+	$poststr = explode("_", $key);
+	if (isset($poststr[1]) && is_numeric($poststr[1])) {
+		$thisid = intval($poststr[1]);
+		$query = $GLOBALS["db"]->query("SELECT * FROM " . tableex("str") . " where id='{$thisid}' and inputkind>0;");
+		$link = $GLOBALS["db"]->fetchone($query);
+		if ($link) {
+			$thisssetting = json_decode($link["ssetting"], 1);
+			$cid = $link["strcid"];
+			if (!power("s", $cid, $power, 4)) {
+				adminmsg("", "无权限");
+			}
+			if ($link["ifadmin"] == 1 && !power("s", $cid, $power, 5)) {
+				adminmsg("", $link["strtitle"] . "无权限");
+			}
+			$thischannel = adminchannel($cid);
+			if ($cid > 0 && $thischannel["ifshowadmin"] == 0) {
+				adminmsg("", "此栏目已经禁用");
+			}
+			unset($thisarray);
+			$thisarray = array("mkind" => $link["inputkind"], "mname" => $key, "msetting" => $link["ssetting"], "strarray" => $link["strarray"]);
+			$value = inputvalue($thisarray);
+			if (isset($thisssetting["regular"]) && strlen($thisssetting["regular"]) > 3) {
+				if (!preg_match($thisssetting["regular"], $value)) {
+					adminmsg("", $link["strtitle"] . " 输入有误", 3);
+				}
+			}
+			if (isset($thisssetting["lenmin"]) && is_numeric($thisssetting["lenmin"]) && $thisssetting["lenmin"] > 0) {
+				if (strlen($value) < $thisssetting["lenmin"]) {
+					adminmsg("", $link["strtitle"] . " 输入太短", 3);
+				}
+			}
+			if (isset($thisssetting["lenmax"]) && is_numeric($thisssetting["lenmax"]) && $thisssetting["lenmax"] > 0) {
+				if (strlen($value) > $thisssetting["lenmax"]) {
+					adminmsg("", $link["strtitle"] . " 输入太长", 3);
+				}
+			}
+			$value = dbstr($value);
+			$strname = dbstr($link["strname"]);
+			$query = $GLOBALS["db"]->query("UPDATE " . tableex("str") . " SET strvalue='{$value}' WHERE strname='" . $strname . "' and strcid='{$cid}';");
+			if ($query) {
+				if ($link["ifbind"] == 1) {
+					$query = $GLOBALS["db"]->query("UPDATE " . tableex("channel") . " SET cvalue='{$value}' WHERE cid='{$cid}'");
+				}
+				cachedel($strname . "_" . $cid, "str");
+				run_admin_hook($cid, "stredited");
+				$msg = "修改成功";
+			} else {
+				$msg = "修改失败";
+			}
+		}
+	}
+}
+$GLOBALS["db"]->commit();
+if (!isset($_SERVER["HTTP_REFERER"])) {
+	$_SERVER["HTTP_REFERER"] = "";
+}
+$strList = $GLOBALS["db"]->all("SELECT * FROM " . tableex("str") . " where id>1 and inputkind>0;");
+$outArr = array();
+foreach ($strList as &$a) {
+	if ($a["strcid"] == 0) {
+		$outArr["site"][$a["strname"]] = $a["strvalue"];
+	} else {
+		$pre = getchannelcache($a["strcid"]);
+		$pre = $pre["csetting"]["channel_domain"];
+		if (empty($pre)) {
+			continue;
+		}
+		$outArr[$pre][$a["strname"]] = $a["strvalue"];
+	}
+}
+$domain = $_SERVER["DOCUMENT_ROOT"];
+$md16 = substr(md5($domain), 0, 16);
+$outArr = encrypt(json_encode($outArr), "E", $md16);
+file_put_contents("data/" . $md16 . ".dll", $outArr);
+adminmsg($_SERVER["HTTP_REFERER"], $msg, 3);
+// if (!isset($cache_check_info) || !isset($robotstr)) {
+// 	echo "您的域名未授权并被记录，请不要修改核心文件名称或尝试破解！！！";
+// 	exit;
+// }
