a95e6c96eafb0d2a62eb965211d78b061b4f3ce2
gcj-master <980227856@qq.com>
2022-03-12 13:26:54 +0800

    优化

--- UatCode/v1/task/collection_task.sh
+++ UatCode/v1/task/collection_task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 定时采集任务
 
-curl "http://149.28.22.163/api.php/timming/index.html?enforce=1&name=hongniu" >> /home/wwwroot/www.feimaoyingshi.com/task/log/hn.txt; 
-curl "http://149.28.22.163/api.php/timming/index.html?enforce=1&name=wujin" >> /home/wwwroot/www.feimaoyingshi.com/task/log/wujin.txt;
-curl "http://149.28.22.163/api.php/timming/index.html?enforce=1&name=uku" >> /home/wwwroot/www.feimaoyingshi.com/task/log/uk.txt;
-curl "http://149.28.22.163/api.php/timming/index.html?enforce=1&name=shandian" >> /home/wwwroot/www.feimaoyingshi.com/task/log/sd.txt;
+curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=hongniu" >> /home/wwwroot/www.feimaoyingshi.com/task/log/hn.txt; 
+curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=wujin" >> /home/wwwroot/www.feimaoyingshi.com/task/log/wujin.txt;
+curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=uku" >> /home/wwwroot/www.feimaoyingshi.com/task/log/uk.txt;
+curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=shandian" >> /home/wwwroot/www.feimaoyingshi.com/task/log/sd.txt;
