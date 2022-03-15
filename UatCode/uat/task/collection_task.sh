7f79fbfce953b701b92d08f187bb301d00f6a90d
gcj-master <980227856@qq.com>
2022-03-15 22:47:40 +0800

    commit

--- UatCode/uat/task/collection_task.sh
+++ UatCode/uat/task/collection_task.sh
@@ -5,3 +5,4 @@ curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=hongniu" >> /ho
 curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=wujin" >> /home/wwwroot/www.feimaoyingshi.com/task/log/wujin.txt;
 curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=uku" >> /home/wwwroot/www.feimaoyingshi.com/task/log/uk.txt;
 curl "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=shandian" >> /home/wwwroot/www.feimaoyingshi.com/task/log/sd.txt;
+crul "http://127.0.0.1/api.php/timming/index.html?enforce=1&name=tk" >> /home/wwwroot/www.feimaoyingshi.com/task/log/tk.txt;
