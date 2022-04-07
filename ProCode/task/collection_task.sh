1d4a2aa148c45ef2b6e73db105ca44189158be8a
gcj-master <980227856@qq.com>
2022-04-07 20:21:15 +0800

    修改任务

--- ProCode/task/collection_task.sh
+++ ProCode/task/collection_task.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # 定时采集任务
 
-curl "http://www.feimaoyingshi.com/api.php/timming/index.html?enforce=1&name=wujin" >> /home/wwwroot/www.feimaoyingshi.com/task/log/hn.txt; 
+curl -k "http://www.feimaoyingshi.com/api.php/timming/index.html?enforce=1&name=wujin" >> /home/wwwroot/www.feimaoyingshi.com/task/log/wujin.txt;
+curl -k "http://www.feimaoyingshi.com/api.php/timming/index.html?enforce=1&name=hn" >> /home/wwwroot/www.feimaoyingshi.com/task/log/hn.txt; 
